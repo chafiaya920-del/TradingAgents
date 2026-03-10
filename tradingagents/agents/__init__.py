a202c9a0a8e698bfcfbb7fc26098a13c6227beb5
aya chafi <chafiaya920@gmail.com>
2026-03-09 19:11:52 -0700

    feat: export create_crypto_analyst

--- tradingagents/agents/__init__.py
+++ tradingagents/agents/__init__.py
@@ -6,6 +6,7 @@ from .analysts.fundamentals_analyst import create_fundamentals_analyst
 from .analysts.market_analyst import create_market_analyst
 from .analysts.news_analyst import create_news_analyst
 from .analysts.social_media_analyst import create_social_media_analyst
+from .analysts.crypto_analyst import create_crypto_analyst, CRYPTO_ANALYST_TOOLS
 
 from .researchers.bear_researcher import create_bear_researcher
 from .researchers.bull_researcher import create_bull_researcher
@@ -36,5 +37,7 @@ __all__ = [
     "create_risk_manager",
     "create_conservative_debator",
     "create_social_media_analyst",
+    "create_crypto_analyst",
+    "CRYPTO_ANALYST_TOOLS",
     "create_trader",
 ]
